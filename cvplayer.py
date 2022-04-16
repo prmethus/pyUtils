f5658425881bc24f4787c107f647862cf7902c77
Sarzil <sarzilh701@gmail.com>
2022-04-16 15:55:19 +0600

    Used Black Formatter

--- cvplayer.py
+++ cvplayer.py
@@ -10,31 +10,45 @@ manual = """Video Window controls:
 'Esc' - quit
 """
 
+
 def show_help():
 
-    open_help = "\nrun 'open PATH' to open an image or video file" 
+    open_help = "\nrun 'open PATH' to open an image or video file"
     if platform.system() == "Linux" or platform.system() == "Darwin":
-        open_example = "example: \'open /home/username/Videos/file.mp4\'"
+        open_example = "example: 'open /home/username/Videos/file.mp4'"
     elif platform.system() == "Windows":
-        open_example = "example: \'open C:\\\\Users\\\\username\\\\Videos\\\\video.mp4\'"
+        open_example = "example: 'open C:\\\\Users\\\\username\\\\Videos\\\\video.mp4'"
 
     print("\n\nCommands: open, set resolution, exit/quit")
-    print(open_help + "\nSupported Formats: {}".format(SUPPORTED_FORMATS) + "\n" + open_example + "\n")
-    print("run \'set resolution WIDTHxHEIGHT\' to change the resolution of video.\nBy default, it's set to 1280x720.\nexample: \'set resolution 1368x768\'\n")
+    print(
+        open_help
+        + "\nSupported Formats: {}".format(SUPPORTED_FORMATS)
+        + "\n"
+        + open_example
+        + "\n"
+    )
+    print(
+        "run 'set resolution WIDTHxHEIGHT' to change the resolution of video.\nBy default, it's set to 1280x720.\nexample: 'set resolution 1368x768'\n"
+    )
     print(manual)
     main()
 
+
 def open_media(command):
-    
+
     global filepath
     command = command.split(" ")
     command.remove("open")
     filepath = " ".join(command)
-    
+
     if len(filepath) == 0:
-        print("\nFILEPATH NOT SPECIFIED.\nPlease re-run \'open' by specifying en existing file path.")
+        print(
+            "\nFILEPATH NOT SPECIFIED.\nPlease re-run 'open' by specifying en existing file path."
+        )
     elif not os.path.exists(filepath):
-        print("\nFILEPATH DOESN\'T EXIST.\nPlease re-run \'open' with an existing file path.")
+        print(
+            "\nFILEPATH DOESN'T EXIST.\nPlease re-run 'open' with an existing file path."
+        )
     else:
         fileformat = filepath.split(".")[-1]
         print(fileformat)
@@ -42,31 +56,41 @@ def open_media(command):
         if fileformat in SUPPORTED_FORMATS:
             cvfuncstart(filepath)
         else:
-            print(f"\nFORMAT NOT SUPPORTED.\nPlease re-run \'open\' with a file the format of which is supported.\nSupported formats{SUPPORTED_FORMATS}")
+            print(
+                f"\nFORMAT NOT SUPPORTED.\nPlease re-run 'open' with a file the format of which is supported.\nSupported formats{SUPPORTED_FORMATS}"
+            )
     main()
+
 
 def set_resolution(command):
     global VIDEO_RESOLUTION
-    
+
     try:
         x = command.split(" ")[-1]
         height = int(x.split("x")[0])
         width = int(x.split("x")[1])
 
         if height > 7680:
-            print("\nMAXIMUM RESOLUTION HEIGHT LIMIT SURPASSED\nPlease re-run \'set resolution\' with the correct values")
+            print(
+                "\nMAXIMUM RESOLUTION HEIGHT LIMIT SURPASSED\nPlease re-run 'set resolution' with the correct values"
+            )
             return
         if width > 4320:
-            print("\nMAXIMUM RESOLUTION WIDTH LIMIT SURPASSED\nPlease re-run \'set resolution\' with the correct values")
+            print(
+                "\nMAXIMUM RESOLUTION WIDTH LIMIT SURPASSED\nPlease re-run 'set resolution' with the correct values"
+            )
             return
-        
+
         VIDEO_RESOLUTION = (height, width)
         print("Resolution set to: {}".format(VIDEO_RESOLUTION))
 
     except Exception:
-        print("\nUNDEFINED INPUT. Please try again.\nexample: \'set resolution 1368x768\'")
-         
+        print(
+            "\nUNDEFINED INPUT. Please try again.\nexample: 'set resolution 1368x768'"
+        )
+
     main()
+
 
 def main():
 
@@ -75,26 +99,26 @@ def main():
         print("\ntype 'help' to show the help menu\n")
         print(manual)
         begin = True
-    
+
     command = input("\n|cvplayer run| ").strip()
-    
+
     if command == ("help"):
-        show_help()  
+        show_help()
     elif command.startswith("open"):
-        open_media(command)  
+        open_media(command)
     elif command == "quit" or command == "exit":
         exit()
     elif command.startswith("set"):
         if command.startswith("set resolution"):
             set_resolution(command)
     else:
-        print("\nCOMMAND NOT RECOGNIZED.\nRun \'help\' to see the commands.")
+        print("\nCOMMAND NOT RECOGNIZED.\nRun 'help' to see the commands.")
         main()
 
-        
-        
+
 def reshape_img(frame, resolution):
     return cv2.resize(frame, resolution, interpolation=cv2.INTER_AREA)
+
 
 def cvfuncstart(filepath):
 
@@ -104,11 +128,11 @@ def cvfuncstart(filepath):
     isPaused = False
     while cap.isOpened():
         try:
-            time.sleep(1/frames)
-            
+            time.sleep(1 / frames)
+
             if not isPaused:
                 ret, frame = cap.read()
-                
+
             frame = reshape_img(frame, VIDEO_RESOLUTION)
             cv2.imshow(filepath, frame)
             x = cv2.waitKeyEx(1)
@@ -131,7 +155,8 @@ def cvfuncstart(filepath):
 
     cv2.destroyAllWindows()
     cap.release()
-    
+
+
 if __name__ == "__main__":
     main()
 
--- onlineimgtopdf.py
+++ onlineimgtopdf.py
@@ -6,13 +6,16 @@ def url_inp():
     urls = []
     i = 1
     while True:
-        inp = input(str(r"|type 'cancel' to cancel| Enter url of image file no.{} :".format(i)))
+        inp = input(
+            str(r"|type 'cancel' to cancel| Enter url of image file no.{} :".format(i))
+        )
         if inp.strip().lower() == "cancel":
             break
         else:
             urls.append(inp)
             i += 1
     return urls
+
 
 def download_imgs(urls):
     img_files = []
@@ -25,22 +28,28 @@ def download_imgs(urls):
             i += 1
     return img_files
 
+
 def images_to_pdf(img_files):
     images = []
     for img_file in img_files:
         newimg = PIL.Image.open(img_file).convert("RGB")
-        imgfile = PIL.Image.new("RGB", newimg.size, (0,0,0))
+        imgfile = PIL.Image.new("RGB", newimg.size, (0, 0, 0))
         imgfile.paste(newimg, mask=newimg.split()[3])
         images.append(imgfile)
-	
-    images[0].save(input(str("Enter PDF file name: ")), save_all=True, append_images=images[1:])
+
+    images[0].save(
+        input(str("Enter PDF file name: ")), save_all=True, append_images=images[1:]
+    )
+
 
 def rm_imgs():
     inp = input(str(r"| Remove the images from your hard drive? [y/n]:"))
+
 
 def main():
     urls = url_inp()
     img_files = download_imgs(urls)
     images_to_pdf(img_files)
+
 
 main()
